08f72d349f821ecf004b719486fbd9df9df380e2
WasapaMamulyam <workhard128@hotmail.com>
2025-01-18 15:18:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,15 @@
-// Функция для расчета зарплаты курьера
-function calculateSalary(job) {
+// Функция для обновления количества рабочих дней в неделю
+function updateWorkingDays(job) {
+    let workingDaysValue = 0;
+
     if (job === 1) {
-        const deliveries = parseInt(document.getElementById("deliveriesJob1").value);
-        document.getElementById("deliveriesJob1Value").innerText = deliveries;
-
-        const workingDays = parseInt(document.getElementById("workingDaysJob1").value);
-        document.getElementById("workingDaysJob1Value").innerText = workingDays;
-
-        const deliveryRate = 2400;
-
-        const dailySalary = deliveries * deliveryRate;
-        const weeklySalary = dailySalary * workingDays;
-        const monthlySalary = weeklySalary * 4;
-        const yearlySalary = monthlySalary * 12;
-
-        document.getElementById("job1DailyTotal").innerText = dailySalary;
-        document.getElementById("job1WeeklyTotal").innerText = weeklySalary;
-        document.getElementById("job1MonthlyTotal").innerText = monthlySalary;
-        document.getElementById("job1YearlyTotal").innerText = yearlySalary;
+        workingDaysValue = document.getElementById("workingDaysJob1").value;
+        document.getElementById("workingDaysJob1Value").innerText = workingDaysValue;
+    } else if (job === 2) {
+        workingDaysValue = document.getElementById("workingDaysJob2").value;
+        document.getElementById("workingDaysJob2Value").innerText = workingDaysValue;
+    } else if (job === 3) {
+        workingDaysValue = document.getElementById("workingDaysJob3").value;
+        document.getElementById("workingDaysJob3Value").innerText = workingDaysValue;
     }
 }
-
-// Функция для обновления ползунков и расчета зарплаты склада
-function updateJob2Form() {
-    const job2Option = document.getElementById("job2Option").value;
-    if (job2Option === "1") {
-        // Город
-        document.getElementById("deliveriesJob2_50").setAttribute("max", 40);
-        document.getElementById("deliveriesJob2_100").setAttribute("max", 40);
-        document.getElementById("deliveriesJob2_200").setAttribute("max", 40);
-        document.getElementById("deliveriesJob2_300").setAttribute("max", 40);
-        document.getElementById("deliveriesJob2_500").setAttribute("max", 40);
-    } else {
-        // Лес
-        document.getElementById("deliveriesJob2_50").setAttribute("max", 30);
-        document.get
